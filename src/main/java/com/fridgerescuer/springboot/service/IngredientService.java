3e506cd5b996086082c8c349ed75659db65af240
overnew <ehdls1303@gmail.com>
2022-08-05 14:17:28 +0900

    feat: ingredient service

---
+++ src/main/java/com/fridgerescuer/springboot/service/IngredientService.java
@@ -0,0 +1,10 @@
+package com.fridgerescuer.springboot.service;
+
+
+import com.fridgerescuer.springboot.data.dao.IngredientDAO;
+import com.fridgerescuer.springboot.data.dto.IngredientDTO;
+import com.fridgerescuer.springboot.data.dto.IngredientResponseDTO;
+
+public interface IngredientService {
+    IngredientResponseDTO saveIngredient(IngredientDTO ingredientDTO);
+}
