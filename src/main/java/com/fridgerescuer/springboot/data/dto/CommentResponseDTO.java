4e5134a89c1efa9fd76a6012411f8b5789691d7c
overnew <ehdls1303@gmail.com>
2022-08-26 14:15:20 +0900

    Feat: Comment Respones/DTO

---
+++ src/main/java/com/fridgerescuer/springboot/data/dto/CommentResponseDTO.java
@@ -0,0 +1,20 @@
+package com.fridgerescuer.springboot.data.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+public class CommentResponseDTO {
+    private String id;
+
+    private double rating;
+    private String body;
+
+    private String imageId;
+    private String date;
+
+    private String recipeId;
+}
